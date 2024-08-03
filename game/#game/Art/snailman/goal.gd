5de79a6106a24a08c42890a6e8ca4c2518e898c0
Ivan-Korsakov <38763216+Ivan-Korsakov@users.noreply.github.com>
2024-08-03 17:50:45 -0300

    Snailman animaton fixed (#7)

--- game/#game/Art/snailman/goal.gd
+++ game/#game/Art/snailman/goal.gd
@@ -3,7 +3,6 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-
 	pass # Replace with function body.
 
 
@@ -11,12 +10,12 @@ func _ready():
 func _process(delta):
 	var child: RigidBody3D
 	child = find_child("RigidBody3D")
-	
+	child.set_contact_monitor(true)
 	if (child):
 		var children = child.get_colliding_bodies()
+		print(children)
 		for objectCollision in children:
 			if objectCollision.name == "Character":
 				print("hmm")
 				Utils.get_scene_manager(self).player_reached_goal()
 				
-	pass
