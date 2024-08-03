f6ab07bcaba9480e24021a8f5d8e3b40dcb35442
Gustavo Vilas Boas <gustavovbs98@gmail.com>
2024-08-03 13:41:10 -0300

    Lose method

--- game/#game/scenes/gameplay_proto/scene_manager.gd
+++ game/#game/scenes/gameplay_proto/scene_manager.gd
@@ -1,10 +1,18 @@
 class_name GameplayProtoSceneManager extends Node3D
 
+
+const main_menu_scene_path = "res://#game/scenes/main_menu/main_menu.tscn"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func player_reached_goal():
+	print("GANHOO")
+	
+func time_over():
+	print("LOST")
+	get_tree().change_scene_to_file(main_menu_scene_path)
