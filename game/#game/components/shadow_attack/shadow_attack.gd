f61d36b475e01fadf0eef211aa06cf8c15b448f9
Arthur Pedroso <arthurnaves@hotmail.com.br>
2024-08-03 23:58:09 -0300

    -progress agent and trail

--- game/#game/components/shadow_attack/shadow_attack.gd
+++ game/#game/components/shadow_attack/shadow_attack.gd
@@ -4,13 +4,18 @@ extends CharacterBody3D
 @export var nav_agent : NavigationAgent3D
 @export var player : CharacterFpc
 
-const SPEED = 100
+const SPEED = 2
+
+func _process(delta):
+	if player != null:
+		nav_agent.target_position = player.position
+	
 
 func _physics_process(delta):
 	if nav_agent.is_target_reachable():
 		if nav_agent.distance_to_target() > 1:
-			var next_waypoint = nav_agent.target_position
+			var next_waypoint = nav_agent.get_next_path_position()
 			var direction = global_position.direction_to(next_waypoint).normalized()
-			global_position += direction * delta * SPEED
-	if player != null:
-		nav_agent.target_position = player.position
+			var new_pos = global_position + (direction * delta * SPEED)
+			new_pos.y =  global_position.y
+			global_position = new_pos
