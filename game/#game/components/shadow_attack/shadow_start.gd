fc48278aab3edec3aaf8ba5c67aa8842a1c821c7
Gustavo Vilas Boas <gustavovbs98@gmail.com>
2024-08-04 18:32:34 -0300

    Implemeted

--- game/#game/components/shadow_attack/shadow_start.gd
+++ game/#game/components/shadow_attack/shadow_start.gd
@@ -3,12 +3,16 @@ extends CharacterBody3D
 @export var nav_region : NavigationRegion3D
 @export var player : CharacterFpc
 
+@export var enemy_manager : EnemyManager
+
 func _ready():
 	var attack = find_child("ShadowAttack")
 	attack.nav_region = nav_region
 	attack.player = player
 
 func kill():
+	if enemy_manager:
+		enemy_manager.on_enemy_killed()
 	queue_free()
 	
 
